cc744b529c22f51ec849eed6aec5eb42ecf44573
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2023-12-21 16:30:51 +0100

    remove try catch

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -4,102 +4,94 @@ import { ImageResponse } from 'next/server'
 export const runtime = 'edge'
 
 export async function GET(request: Request) {
-  try {
-    const { searchParams } = new URL(request.url)
+  const { searchParams } = new URL(request.url)
 
-    const hasTitle = searchParams.has('title')
-    const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'Default title'
+  const hasTitle = searchParams.has('title')
+  const title = hasTitle ? searchParams.get('title')?.slice(0, 100) : 'Default title'
 
-    return new ImageResponse(
-      (
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          backgroundImage: `url(${siteMetadata.siteUrl}/static/images/social-card-bg.jpg)`,
+          backgroundSize: '100% 100%',
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          padding: '40px 80px',
+        }}
+      >
         <div
           style={{
-            backgroundImage: `url(${siteMetadata.siteUrl}/static/images/social-card-bg.jpg)`,
-            backgroundSize: '100% 100%',
-            height: '100%',
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            justifyContent: 'center',
-            padding: '40px 80px',
+            fontSize: 60,
+            fontWeight: 800,
+            letterSpacing: '-0.025em',
+            lineHeight: 1,
+            color: 'white',
+            marginBottom: 24,
+            whiteSpace: 'pre-wrap',
           }}
         >
-          <div
-            style={{
-              fontSize: 60,
-              fontWeight: 800,
-              letterSpacing: '-0.025em',
-              lineHeight: 1,
-              color: 'white',
-              marginBottom: 24,
-              whiteSpace: 'pre-wrap',
-            }}
-          >
-            {title}
-          </div>
+          {title}
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}
+        >
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            alt=""
+            width="80"
+            height="80"
+            src={`https://avatars.githubusercontent.com/u/311343?v=4`}
+          />
           <div
             style={{
               display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
+              flexDirection: 'column',
             }}
           >
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img
-              alt=""
-              width="80"
-              height="80"
-              src={`https://avatars.githubusercontent.com/u/311343?v=4`}
-            />
             <div
               style={{
-                display: 'flex',
-                flexDirection: 'column',
+                fontSize: 30,
+                fontWeight: 800,
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                color: 'white',
+                marginLeft: 20,
+                marginBottom: 10,
+                whiteSpace: 'pre-wrap',
               }}
             >
-              <div
-                style={{
-                  fontSize: 30,
-                  fontWeight: 800,
-                  letterSpacing: '-0.025em',
-                  lineHeight: 1,
-                  color: 'white',
-                  marginLeft: 20,
-                  marginBottom: 10,
-                  whiteSpace: 'pre-wrap',
-                }}
-              >
-                {siteMetadata.author}
-              </div>
-              <div
-                style={{
-                  fontSize: 20,
-                  fontWeight: 800,
-                  letterSpacing: '-0.025em',
-                  lineHeight: 1,
-                  color: 'white',
-                  marginLeft: 20,
-                  whiteSpace: 'pre-wrap',
-                }}
-              >
-                {siteMetadata.siteUrl}
-              </div>
+              {siteMetadata.author}
+            </div>
+            <div
+              style={{
+                fontSize: 20,
+                fontWeight: 800,
+                letterSpacing: '-0.025em',
+                lineHeight: 1,
+                color: 'white',
+                marginLeft: 20,
+                whiteSpace: 'pre-wrap',
+              }}
+            >
+              {siteMetadata.siteUrl}
             </div>
           </div>
         </div>
-      ),
-      {
-        width: 1200,
-        height: 630,
-        debug: true,
-      }
-    )
-  } catch (e: unknown) {
-    console.error('here')
-    console.error(e)
-    return new Response(`Failed to generate the image`, {
-      status: 500,
-    })
-  }
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      debug: true,
+    }
+  )
 }
