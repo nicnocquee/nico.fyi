a2a930dc68f8c634eddb0a7ca91b09a23200945a
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-02-23 17:36:30 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -86,6 +87,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             </div>
           </SectionContainer>
         </ThemeProviders>
+        <VercelAnalytics />
       </body>
     </html>
   )
