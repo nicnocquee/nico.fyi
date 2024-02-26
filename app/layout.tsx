4b24872d0a4bacb08f73551e9949b8614e90fbb3
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-02-26 10:29:30 +0100

    add vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -88,6 +89,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </SectionContainer>
         </ThemeProviders>
         <VercelAnalytics />
+        <SpeedInsights />
       </body>
     </html>
   )
