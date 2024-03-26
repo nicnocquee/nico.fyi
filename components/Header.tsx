499bd815ac34cdd17b2cecd1aaae67cae35b3337
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-03-26 23:53:26 +0100

    add shadcn

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ const Header = () => {
             <Link
               key={link.title}
               href={link.href}
-              className="hidden font-medium text-gray-900 sm:block dark:text-gray-100"
+              className="hidden font-medium text-gray-900 dark:text-gray-100 sm:block"
             >
               {link.title}
             </Link>
