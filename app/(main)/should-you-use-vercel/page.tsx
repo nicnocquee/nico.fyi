de5a71c97791e4bef0860a719fee6f0c8242f42f
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-04-07 15:00:39 +0200

    use different twitter image for yes or no

--- app/(main)/should-you-use-vercel/page.tsx
+++ app/(main)/should-you-use-vercel/page.tsx
@@ -1,26 +1,42 @@
-import { Metadata } from 'next'
+import { Metadata, ResolvingMetadata } from 'next'
 import Flowchart from './content'
 import OGImage from './ogimage.jpg'
+import OGImageYes from './ogimage-yes.jpg'
+import OGImageNo from './ogimage-no.jpg'
 
-export const metadata: Metadata = {
-  title: 'Should you use Vercel?',
-  description: `Vercel recently updated their pricing model which caused some uproar among developers. Answer the following questions to find out if you should use Vercel or not.`,
-  keywords: ['vercel', 'pricing', 'model', 'uproar', 'developers'],
-  creator: 'Nico Prananta',
-  openGraph: {
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
+  const image = searchParams
+    ? searchParams.use
+      ? searchParams.use === '1'
+        ? OGImageYes
+        : OGImageNo
+      : OGImage
+    : OGImage
+  return {
     title: 'Should you use Vercel?',
     description: `Vercel recently updated their pricing model which caused some uproar among developers. Answer the following questions to find out if you should use Vercel or not.`,
-    url: 'https://www.nico.fyi/articles/should-you-use-vercel',
-    siteName: 'nico.fyi',
-    locale: 'en_US',
-    type: 'website',
-    images: [OGImage.src],
-  },
-  twitter: {
-    title: 'Should you use Vercel?',
-    card: 'summary_large_image',
-    images: [OGImage.src],
-  },
+    keywords: ['vercel', 'pricing', 'model', 'uproar', 'developers'],
+    creator: 'Nico Prananta',
+    openGraph: {
+      title: 'Should you use Vercel?',
+      description: `Vercel recently updated their pricing model which caused some uproar among developers. Answer the following questions to find out if you should use Vercel or not.`,
+      url: 'https://www.nico.fyi/articles/should-you-use-vercel',
+      siteName: 'nico.fyi',
+      locale: 'en_US',
+      type: 'website',
+      images: [image.src],
+    },
+    twitter: {
+      title: 'Should you use Vercel?',
+      card: 'summary_large_image',
+      images: [image.src],
+    },
+  }
 }
 
 const FlowchartPage = () => {
