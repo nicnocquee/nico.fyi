b9ebf64b2f218494066b6e8c7c29e7bc49d769b7
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-05-08 16:37:47 +0200

    add number of posts

--- app/(other-pages)/insights/page.tsx
+++ app/(other-pages)/insights/page.tsx
@@ -1,4 +1,16 @@
+import { allBlogs } from '@/.contentlayer/generated'
 import siteMetadata from '@/data/siteMetadata'
+import {
+  endOfMonth,
+  endOfWeek,
+  format,
+  isAfter,
+  startOfMonth,
+  startOfWeek,
+  subDays,
+  subMonths,
+  subWeeks,
+} from 'date-fns'
 
 export const generateMetadata = async () => {
   return {
@@ -27,7 +39,29 @@ export const generateMetadata = async () => {
   }
 }
 
+export const revalidate = 86400
+
 export default function InsightsPage() {
+  const thisWeekStart = startOfWeek(new Date(), { weekStartsOn: 1 })
+  const thisWeekEnd = endOfWeek(thisWeekStart)
+  const thisWeek = `${format(thisWeekStart, 'yyyy-MM-dd')} - ${format(thisWeekEnd, 'dd')}`
+  const thisMonthStart = startOfMonth(new Date())
+  const thisMonthEnd = endOfMonth(thisMonthStart)
+  const thisMonth = `${format(thisMonthStart, 'yyyy-MM-dd')} - ${format(thisMonthEnd, 'dd')}`
+  const lastWeekStart = subWeeks(thisWeekStart, 1)
+  const lastWeekEnd = endOfWeek(lastWeekStart)
+  const lastWeek = `${format(lastWeekStart, 'yyyy-MM-dd')} - ${format(lastWeekEnd, 'dd')}`
+  const lastMonthStart = subMonths(thisMonthStart, 1)
+  const lastMonthEnd = endOfMonth(lastMonthStart)
+  const lastMonth = `${format(lastMonthStart, 'yyyy-MM-dd')} - ${format(lastMonthEnd, 'dd')}`
+  const numberOfPostsThisWeek = allBlogs.filter((b) => isAfter(b.date, thisWeekStart)).length
+  const numberOfPostsLastWeek = allBlogs.filter(
+    (b) => isAfter(b.date, lastWeekStart) && !isAfter(b.date, thisWeekStart)
+  ).length
+  const numberOfPostsThisMonth = allBlogs.filter((b) => isAfter(b.date, thisMonthStart)).length
+  const numberOfPostsLastMonth = allBlogs.filter(
+    (b) => isAfter(b.date, lastMonthStart) && !isAfter(b.date, thisMonthStart)
+  ).length
   return (
     <div className="flex flex-col space-y-8">
       <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
@@ -36,6 +70,33 @@ export default function InsightsPage() {
       <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
         This is a page for nico.fyi insights.
       </p>
+
+      <div className="flex flex-col space-y-4">
+        <h2 className="text-xl font-bold">
+          <a id={'posts-stats'} href={`#posts-stats`}>
+            {'Number of posts'}
+          </a>
+        </h2>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-4">
+          {[
+            [numberOfPostsThisWeek, 'This week', thisWeek],
+            [numberOfPostsLastWeek, 'Last week', lastWeek],
+            [numberOfPostsThisMonth, 'This month', thisMonth],
+            [numberOfPostsLastMonth, 'Last month', lastMonth],
+          ].map(([total, label, range], i) => {
+            return (
+              <div
+                key={i}
+                className="flex flex-col items-center space-y-4 rounded-md border border-gray-200 p-4 shadow-md"
+              >
+                <p className="text-7xl font-bold">{total}</p>
+                <p>{label}</p>
+                <p className="text-sm font-extralight text-gray-500">{range}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
 
       {[
         [
