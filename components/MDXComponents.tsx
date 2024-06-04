c61d20afc00e4e9ae12b5a4ad054fb2e1f345819
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-06-04 15:30:40 +0200

    new post

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -2,14 +2,14 @@ import TOCInline from 'pliny/ui/TOCInline'
 import CustomPre from './Pre'
 import BlogNewsletterForm from 'pliny/ui/BlogNewsletterForm'
 import type { MDXComponents } from 'mdx/types'
-import Image from './Image'
 import CustomLink from './Link'
 import AutocompleteSection from './autocomplete-section'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { LanguageSelectDemo } from './language-select-demo'
+import ZoomableImage from './zoomable-image'
 
 export const components: MDXComponents = {
-  Image,
+  Image: ZoomableImage,
   TOCInline,
   a: CustomLink,
   pre: CustomPre,
