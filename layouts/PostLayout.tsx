6167244616f223a73037d7c5ab01f0c1139b17e7
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-07-04 21:42:49 +0200

    add looking for job

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -9,6 +9,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 const editUrl = (path: string) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path: string) =>
@@ -58,7 +59,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
             <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
-              <dd>
+              <dd className="space-y-4">
                 <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
                   {authorDetails.map((author) => (
                     <li className="flex items-center space-x-2" key={author.name}>
@@ -81,7 +82,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                               href={author.twitter}
                               className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                             >
-                              {author.twitter.replace('https://twitter.com/', '@')}
+                              Follow me: {author.twitter.replace('https://twitter.com/', '@')}
                             </Link>
                           )}
                         </dd>
@@ -110,6 +111,23 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
                   <Comments slug={slug} />
                 </div>
               )}
+              <Card className="bg-slate-100">
+                <CardHeader>
+                  <CardTitle>Did you like this post?</CardTitle>
+                </CardHeader>
+                <CardContent className="overflow-hidden">
+                  <div className="flex flex-col space-y-4">
+                    <p>
+                      I'm looking for a job as full stack developer. If you're interested, you can
+                      read more about me{' '}
+                      <Link className=" text-primary-500 underline" href="/about">
+                        here
+                      </Link>
+                      .
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
             </div>
             <footer>
               <div className="divide-gray-200 text-sm font-medium leading-5 dark:divide-gray-700 xl:col-start-1 xl:row-start-2 xl:divide-y">
