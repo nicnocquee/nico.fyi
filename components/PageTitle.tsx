e062bdf69dcc62fd59bfae377d1d241112caf569
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-09-28 10:46:31 +0200

    update PageTitle component to use new 'text-pretty' class for improved styling

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function PageTitle({ children }: Props) {
   return (
-    <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 duration-500 ease-elastic-in-out-5 hover:scale-150 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+    <h1 className="text-pretty text-3xl font-extrabold leading-9 tracking-tight text-gray-900 duration-500 ease-elastic-in-out-5 hover:scale-150 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
       {children}
     </h1>
   )
