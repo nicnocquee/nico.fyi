522529f975fc9c0ec1e1eb89a6e360e8f9c86a97
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2025-07-10 23:05:20 +0200

    update opengraph

--- app/(mdx-pages)/works/layout.tsx
+++ app/(mdx-pages)/works/layout.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Works',
+  description:
+    "These are some of the projects I've worked on in the Swift Management AG and Hyperjump Technology.",
+}
+
 export default function ProseLayout({ children }: { children: React.ReactNode }) {
   return <main className="prose max-w-none pb-8 dark:prose-invert">{children}</main>
 }
